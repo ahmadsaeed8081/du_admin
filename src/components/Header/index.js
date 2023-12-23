255e45ff2158e9ed0a42046410f7ce752a4be895
ahmad <ahmadsaeed8081@gmail.com>
2023-12-23 13:45:28 +0500

    mobile connect button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/reducers/authReducer";
-
+// import { Link, NavLink } from "react-router-dom";
+import {
+  LogoutIcon,
+  DashboardIcon,
+  CardIcon,
+  SyncIcon,
+} from "../../assets/Icons";
+import { useWeb3Modal } from '@web3modal/react'
+import { useAccount, useDisconnect } from 'wagmi'
 const Header = ({ openSidebar, setOpenSidebar }) => {
   const dispatch = useDispatch();
+  const { address, isConnected,isDisconnected } = useAccount()
+  const { open, close } = useWeb3Modal();
 
   const userLogout = () => {
     dispatch(logout("admin-token"));
@@ -21,6 +31,14 @@ const Header = ({ openSidebar, setOpenSidebar }) => {
           </div>
         </div>
         <div className="right flex items-center justify-end"></div>
+        <div className="side-footer flex items-center  justify-center">
+          <button className="btn-logout button">
+            <h1 className="btn-icon">
+              {/* <LogoutIcon /> */}
+            </h1>
+            <h1 className="btn-lbl" onClick={open} >{isConnected?('Disconnect'): ('Connect Wallet') }</h1>
+          </button>
+        </div>
       </div>
     </div>
   );
